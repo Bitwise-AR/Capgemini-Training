3e226521632b90480dea8d87416d8bb82676a19f
bitwise-ar <rajayush1412@gmail.com>
2026-01-03 09:41:44 +0530

    updates

--- day11/Program.cs
+++ day11/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void main()
+    {
+        using (ResourceHandler handler = new ResourceHandler())
+        {
+            // Use the resource
+            Console.WriteLine("Using resource...");
+        } // Dispose() is called automatically here
+
+        Console.WriteLine("End of Program.");
+    }
+}
---
+++ day12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+class Program{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Hello");
+        sb.Append(" ");
+        sb.Append("World");
+        Console.WriteLine(sb.ToString());
+    }
+}
