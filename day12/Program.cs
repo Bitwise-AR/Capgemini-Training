baaaea7681d715fc719d6dbee0cb5e65d401aa50
bitwise-ar <rajayush1412@gmail.com>
2026-01-06 18:04:17 +0530

    updates

--- day12/Program.cs
+++ day12/Program.cs
@@ -1,28 +1,9 @@
 ﻿using System.Text;
 
 class Program{
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        StringBuilder sb = new StringBuilder();
-        sb.Append("Hello");
-        sb.Append(" ");
-        sb.Append("World");
-        Console.WriteLine(sb.ToString());
-
-        StringBuilder sb1 = new StringBuilder("Hello");
-        StringBuilder sb2 = new StringBuilder("Hello");
-        Console.WriteLine(sb1.Equals(sb2));
-        Console.WriteLine(object.ReferenceEquals(sb1, sb2));
-
-        StringBuilder sb3 = sb2;
-        Console.WriteLine(sb3.Equals(sb2));
-        Console.WriteLine(object.ReferenceEquals(sb3, sb2));
-
-        Console.WriteLine(sb1 == sb2);
-
-        String str1 = "Hello";
-        String str2 = "Hello";
-        Console.WriteLine(str1 == str2);
-        Console.WriteLine(str1.Equals(str2));
+        StringOperations strOps = new StringOperations();
+        strOps.Run();
     }
 }
